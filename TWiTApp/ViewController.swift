0ad5a0250ec398801966fbb18a0bc40a3afc568e
Jeremy Lee <jeremy@jeremylee.co.nz>
2020-06-07 16:51:52 +0200

    Initial Commit

---
+++ TWiTApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TWiTApp
+//
+//  Created by Jeremy Lee on 7/06/20.
+//  Copyright © 2020 Jeremy Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
